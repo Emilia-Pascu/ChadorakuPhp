cfcb31778252c44c830f8dbd5e327cb327eb950f
Pascal Lysaught <pascallysaught@gmail.com>
2017-10-15 15:52:27 -0400

    css mis à jour et listeProduits

--- php/listeProduits.php
+++ php/listeProduits.php
@@ -6,15 +6,37 @@
 
 <?php
     global $connexion;                    
-    $rep = "";
+    $rep = "";     
+    $rep.='<div class="ftr-matcha">';
+    $rep.= '<div class="container">';
+    $rep.= '<div class="row"><div class="col-xs-12">';
+    
     if(!empty($_GET["categorie"])){
         $categorie = $_REQUEST['categorie'];
         $requete="SELECT * FROM produit WHERE categorie = ".$categorie;
+        //entête
+   
+    $rep.= '<h1 class="text-muted">'.$categorie.'</h1>';
+    $rep.='<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div></div></div></div>';
+    $rep.='<div class="container padded">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2>Notre sélection de'.$categorie.'</h2>
+            <hr>
+        </div>
+    </div>';
+    //fin entête
     }else{
-        $requete="SELECT * FROM produit ORDER BY idProduit DESC";    
+        $requete="SELECT * FROM produit ORDER BY idProduit DESC";
+        //entête   
+    $rep.= '<h1 class="text-muted">Tous nos Thés</h1>';
+    $rep.='<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div></div></div></div>';
+    //fin entête    
     }  
                                  
     $dossier="../pochette/";
+    
+
     try{
         $listeFilms=mysqli_query($connexion,$requete);
         $rows=mysqli_num_rows($listeFilms);// nb total d'enregistrements de la base des données
@@ -28,15 +50,19 @@
             $col_class = 'col-sm-3'; // classe col
             
             $rep.= '<div class="'.$container_class.'">';    // ouvrir conteneur
-            while($ligne=mysqli_fetch_object($listeFilms)){                                
+            // $ligne=mysqli_fetch_object($listeFilms);
+            
+            while($ligne=mysqli_fetch_object($listeFilms)){
+                                                
                 if(($counter % $cols) == 1) {    // vérifier si on a un row nouveau
                     $rep.= '<div class="'.$row_class.'">';	// commence row nouveau
                 }
                 $rep.='<div class="'.$col_class.'"><form method="post" id="fProd" name="fProd" action="panier.php?action=add&nom='.$ligne->nom.'">';
-                $rep.='<div class="thumbnail"><a href="detailProduit.php?idProduit='.$ligne->idProduit.'"><img src="'.$dossier.$ligne->image.'"';
-                $rep.=' class="img-responsive" ></a>';               
-                $rep.='<div class="caption"><h5><strong>'.$ligne->nom.'</strong></h5>';
-                $rep.='<h5>'.$ligne->categorie.'</h5><h5><em>$ '.$ligne->prix.'</em></h5>';
+                $rep.='<div ><a href="detailProduit.php?idProduit='.$ligne->idProduit.'"><img src="'.$dossier.$ligne->image.'"';
+                $rep.=' class=" img-circle img-responsive" ></a>';               
+                $rep.='<div class="caption"><h3><strong>'.$ligne->nom.'</strong></h3>';
+                $rep.='<h5>Prix pour 100g: <em>$ '.$ligne->prix.'</em></h5>';
+
                 $rep.='<h5 class="client"><select id="quantite" name="quantite" ><option value="1">1</option><option value="2">2</option>';
                 $rep.='<option value="3">3</option></select></h5><h5 class="client"><button type="submit" class="btn-success"><i class="fa fa-cart-plus"></i> Ajouter</button></h5></div></div></form></div>'; 
                                 
