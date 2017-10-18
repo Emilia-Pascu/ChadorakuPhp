621b4d5d0a9759b363370dccc8d508eea68b1331
Emilia Pascu <emilia_pascu@yahoo.com>
2017-10-18 16:03:22 -0400

    validations enreg + connexion

--- php/connexion.php
+++ php/connexion.php
@@ -5,50 +5,68 @@
 
     global $connexion;      
     $courriel = $motPasse = $message = "";  
-    if (!empty($_POST)) {   
-            $courriel = test_input($_POST["courriel"]);
-            $motPasse = test_input($_POST["motPasse"]);
-    }     
-            if(count($_POST)>0){
-                $requete="SELECT * FROM utilisateur WHERE courriel=? AND motPasse=?";
-                $stmt = $connexion->prepare($requete);
-                $stmt->bind_param("ss", $courriel, $motPasse);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                $count  = mysqli_num_rows($result);
-                if($count>0){
-                    if($ligne = $result->fetch_object()){
-                        $_SESSION["SESS_idUtilisateur"] = $ligne->idUtilisateur;
-                        $_SESSION["SESS_courriel"] = $ligne->courriel;
-                        $_SESSION["SESS_motPasse"] = $ligne->motPasse;
-                        $_SESSION["SESS_nom"] = $ligne->nom;
-                        $_SESSION["SESS_prenom"] = $ligne->prenom;
-                        $_SESSION["SESS_telephone"] = $ligne->telephone;                        
-                        $_SESSION["SESS_noCivique"] = $ligne->noCivique;
-                        $_SESSION["SESS_rue"] = $ligne->rue;
-                        $_SESSION["SESS_appartement"] = $ligne->appartement;  
-                        $_SESSION["SESS_codePostal"] = $ligne->codePostal;
-                        $_SESSION["SESS_ville"] = $ligne->ville;
-                        $_SESSION["SESS_province"] = $ligne->province;
-                        $_SESSION["SESS_pays"] = $ligne->pays;
-                        $_SESSION["SESS_categorie"] = $ligne->categorie;                                      
-                        $stmt->close();
-                        @mysqli_close($connexion);
-
-                        if($_SESSION["SESS_categorie"]=='admin'){
-                            header("Location: gestion.php");
-                        }else{
-                            header("Location: ../index.php");
-                        }                                
+    $isValid = true;
+    if (!empty($_POST)) {  
+        if (empty($_POST["courriel"])) {
+                    $message .= "Le courriel est requis<br/>";
+                    $isValid = false;
+                } else {
+                    $courriel = test_input($_POST["courriel"]);              
+                    if (!preg_match("/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i",$courriel)) {
+                        $message .= "Entrez le courriel dans le format spécifié<br/>"; 
+                        $isValid = false;
                     }
-                }else {
-                    $message = "Nom usager ou mot de passe invalide!";
-                    @mysqli_close($connexion);
                 }
-            }                                         
-           
-    
-    
+        if (empty($_POST["motPasse"])) {
+                    $message .= "Le mot de passe est requis<br/>";
+                    $isValid = false;
+                } else {
+                    $motPasse = test_input($_POST["motPasse"]);          
+                    if (!preg_match("/^[A-Za-zÀ-ÿ0-9!@#$%^&*()_]{5,20}$/",$motPasse)) {
+                        $message .= "Entrez le mot de passe dans le format spécifié<br/>"; 
+                        $isValid = false;
+                    }
+                }
+
+                 if($isValid){
+                    $requete="SELECT * FROM utilisateur WHERE courriel=? AND motPasse=?";
+                    $stmt = $connexion->prepare($requete);
+                    $stmt->bind_param("ss", $courriel, $motPasse);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $count  = mysqli_num_rows($result);
+                    if($count>0){
+                        if($ligne = $result->fetch_object()){
+                            $_SESSION["SESS_idUtilisateur"] = $ligne->idUtilisateur;
+                            $_SESSION["SESS_courriel"] = $ligne->courriel;
+                            $_SESSION["SESS_motPasse"] = $ligne->motPasse;
+                            $_SESSION["SESS_nom"] = $ligne->nom;
+                            $_SESSION["SESS_prenom"] = $ligne->prenom;
+                            $_SESSION["SESS_telephone"] = $ligne->telephone;                        
+                            $_SESSION["SESS_noCivique"] = $ligne->noCivique;
+                            $_SESSION["SESS_rue"] = $ligne->rue;
+                            $_SESSION["SESS_appartement"] = $ligne->appartement;  
+                            $_SESSION["SESS_codePostal"] = $ligne->codePostal;
+                            $_SESSION["SESS_ville"] = $ligne->ville;
+                            $_SESSION["SESS_province"] = $ligne->province;
+                            $_SESSION["SESS_pays"] = $ligne->pays;
+                            $_SESSION["SESS_categorie"] = $ligne->categorie;                                      
+                            $stmt->close();
+                            @mysqli_close($connexion);
+
+                            if($_SESSION["SESS_categorie"]=='admin'){
+                                header("Location: gestion.php");
+                            }else{
+                                header("Location: ../index.php");
+                            }                                
+                        }
+                    }else {
+                        $message = '<div id="msgConn">Nom usager ou mot de passe invalide!</div>';
+                        @mysqli_close($connexion);
+                    }
+                 }             
+    }    
+
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -71,17 +89,17 @@
                         </div>
                         <div class="panel-body">
                             <div class="row">
-                                <form id="formConnexion" action="" enctype="" method="POST">
+                                <form id="formConnexion" action="" enctype="" method="POST" onSubmit="return validerFormConnexion();">
                                     <div class="col-sm-12">
                                         <div class="form-group">
                                             <label for="courriel"><h3>Courriel</h3></label>
-                                            <h3><input class="form-control input-lg" type="text" id="courriel" name="courriel" value=""></h3>
+                                            <h3><input class="form-control input-lg" type="text" id="courriel" name="courriel" value="" required></h3>
                                         </div>
                                     </div>
                                     <div class="col-sm-12">
                                         <div class="form-group">
                                             <label for="motPasse"><h3>Mot de passe</h3></label>
-                                            <h4><input class="form-control input-lg" type="text" id="motPasse" name="motPasse" value=""></h4>
+                                            <h4><input class="form-control input-lg" type="text" id="motPasse" name="motPasse" value="" required></h4>
                                         </div>
                                     </div>
                                     <div class="col-sm-4 col-sm-offset-4" id="btnEnregistrer">
@@ -89,7 +107,7 @@
                                     </div>
                                 </form>                                
                             </div>
-                            <div class="col-sm-12 alert alert-success msg" > <?php echo $message;?></div>
+                            <div class="col-sm-12 alert alert-success msg" id="msg"> <?php echo $message;?></div>
                         </div>
                     </div>
                 </div>
