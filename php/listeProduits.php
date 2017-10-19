1bdf1c1c5b58c8615c7a4cae4d65cd85fdd39e96
Pascal Lysaught <pascallysaught@gmail.com>
2017-10-19 11:47:22 -0400

    police produit individuel

--- php/listeProduits.php
+++ php/listeProduits.php
@@ -17,14 +17,14 @@
         //entête
    
     $rep.= '<h1 class="text-muted">Notre sélection de '.$categorie.'</h1>';
-    $rep.='<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div></div></div></div>';
+    $rep.='</div></div></div></div>';
     
     //fin entête
     }else{
         $requete="SELECT * FROM produit ORDER BY nom ASC";
         //entête   
     $rep.= '<h1 class="text-muted">Tous nos Thés</h1>';
-    $rep.='<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+    
     $rep.='</div></div></div></div>';
     //fin entête    
     }  
